a5e413b9ad1a777e90f518eac572b3b6a37ec91f
Adam Ruman <469068@muni.cz>
2021-05-18 00:28:34 +0200

    adds tests for cyclo complexity check

---
+++ src/test/java/checks/CycloComplexityTest.java
@@ -0,0 +1,269 @@
+package checks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class CycloComplexityTest {
+
+    private final ASTBundle astBundle = new ASTBundle();
+
+    @Test
+    public void testOneCycleViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.forAST);
+        check.leaveToken(astBundle.forAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneCycleDoesNotViolateLimitTwo() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(2);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.forAST);
+        check.leaveToken(astBundle.forAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertFalse(check.isViolationDetected());
+    }
+
+    @Test
+    public void variableDefDoesNotCount() {
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.variableAST);
+        check.leaveToken(astBundle.variableAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertFalse(check.isViolationDetected());
+    }
+
+    @Test
+    public void outsideMethodNotCount() {
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.leaveToken(astBundle.methodAST);
+        check.visitToken(astBundle.variableAST);
+        check.leaveToken(astBundle.variableAST);
+
+        assertFalse(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneWhileViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.whileAST);
+        check.leaveToken(astBundle.whileAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneIfViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.ifAST);
+        check.leaveToken(astBundle.ifAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneDoViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.doAST);
+        check.leaveToken(astBundle.doAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneSwitchViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+        check.setSwitchBlockAsSingleDecisionPoint(true);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.switchAST);
+        check.leaveToken(astBundle.switchAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneCaseViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.caseAST);
+        check.leaveToken(astBundle.caseAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneCatchViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.catchAST);
+        check.leaveToken(astBundle.catchAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneQuestionViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.questionAST);
+        check.leaveToken(astBundle.questionAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneLANDViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.landAST);
+        check.leaveToken(astBundle.landAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testOneLORViolatesLimitOne() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.methodAST);
+        check.visitToken(astBundle.lorAST);
+        check.leaveToken(astBundle.lorAST);
+        check.leaveToken(astBundle.methodAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testConstructorChecked() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.constructorAST);
+        check.visitToken(astBundle.forAST);
+        check.leaveToken(astBundle.forAST);
+        check.leaveToken(astBundle.constructorAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testInitializationChecked() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.initAST);
+        check.visitToken(astBundle.forAST);
+        check.leaveToken(astBundle.forAST);
+        check.leaveToken(astBundle.initAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testStaticInitializationChecked() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.staticInitAST);
+        check.visitToken(astBundle.forAST);
+        check.leaveToken(astBundle.forAST);
+        check.leaveToken(astBundle.staticInitAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+
+    @Test
+    public void testNestedMethods() {
+
+        CycloComplexityCheck check = new CycloComplexityCheck();
+
+        check.setMax(1);
+
+        check.visitToken(astBundle.constructorAST);
+        check.visitToken(astBundle.forAST);
+        check.leaveToken(astBundle.forAST);
+        check.visitToken(astBundle.methodAST);
+        check.leaveToken(astBundle.methodAST);
+        assertFalse(check.isViolationDetected());
+        check.leaveToken(astBundle.constructorAST);
+
+        assertTrue(check.isViolationDetected());
+    }
+}
